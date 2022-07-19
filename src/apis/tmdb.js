bf37b737908b276ea050901e63ee0f30780c13ef
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-07-20 00:30:18 +0700

    add key

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_TMDB_KEY;
+// const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const API_KEY = "2e168b3b879ec814fa52ec1be986b659";
 const baseUrl = 'https://api.themoviedb.org/3/';
 
 const tmdb = axios.create({
